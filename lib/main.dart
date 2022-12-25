7e1c08895c18f6a8d4958021e8c9e6a637150699
Harsha <harshaswift89@gmail.com>
2022-12-25 13:52:54 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+  home: NinjaCard(),
+));
+
+class NinjaCard extends StatefulWidget {
+  const NinjaCard({Key? key}) : super(key: key);
+
+  @override
+  State<NinjaCard> createState() => _NinjaCardState();
+}
+
+class _NinjaCardState extends State<NinjaCard> {
+
+  int ninjaLevel = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: const Text('Ninja ID Card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            ninjaLevel += 1;
+          });
+        },
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/ninja.jpg'),
+                radius: 40.0,
+              ),
+            ),
+
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+
+            const Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+                fontSize: 15.0,
+              ),
+            ),
+
+            const SizedBox(height: 10.0),
+
+            const Text(
+              'Crap Bag',
+              style: TextStyle(
+                color: Colors.limeAccent,
+                letterSpacing: 2.0,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 30.0),
+
+            const Text(
+              'NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+                fontSize: 15.0,
+              ),
+            ),
+
+            const SizedBox(height: 10.0),
+
+            Text(
+              '$ninjaLevel',
+              style: const TextStyle(
+                color: Colors.limeAccent,
+                letterSpacing: 2.0,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 30.0),
+
+
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+
+                const SizedBox(width: 10.0),
+
+                Text(
+                  'CrapBag@121gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 20.0,
+                    letterSpacing: 1.0,
+                  ),
+
+                ),
+              ],
+            ),
+
+          ],
+
+
+        ),
+      ),
+
+    );
+  }
+}
+
